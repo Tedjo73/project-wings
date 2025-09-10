9073df3e86786113f2cdd7418f33838adef36370
Tedjo73 <charlesthato04@gmail.com>
2025-09-10 21:41:48 +0200

    Update Sales.js

--- frontend/src/pages/Sales.js
+++ frontend/src/pages/Sales.js
@@ -29,8 +29,6 @@ const handleSell = async (e) => {
       return alert(`Not enough stock. Only ${product.quantity} left.`);
     }
 
-    const res = await createSale(sale.productId, sale.quantity);
-    alert("Sale recorded");
     setSale({ productId: "", quantity: 1 });
 
     setProducts(await getProducts());
